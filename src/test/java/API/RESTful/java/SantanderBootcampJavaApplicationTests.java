e2877e2b27df2756561c677c1c33711b7b416b3f
Guimathis <guimathias1906@gmail.com>
2024-07-12 15:01:17 -0400

    first commit

---
+++ src/test/java/API/RESTful/java/SantanderBootcampJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package API.RESTful.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
